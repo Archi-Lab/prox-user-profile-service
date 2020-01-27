28a54763d9b6879765ba05a4fe7a4fd781558159
mmarienh <Marco.Marienhagen@googlemail.com>
2020-01-27 16:44:42 +0100

    Repository funktionen angepasst

--- src/main/java/io/archilab/prox/userprofileservice/professor/ProfessorRepository.java
+++ src/main/java/io/archilab/prox/userprofileservice/professor/ProfessorRepository.java
@@ -4,16 +4,17 @@ import io.archilab.prox.userprofileservice.professor.attr.PersonName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+public interface ProfessorRepository extends JpaRepository<Professor, UUID> {
 
     Optional<Professor> findById(@Param(value = "id") UUID id);
-    List<Professor> findByName(@Param(value = "name")PersonName name);
+    List<Professor> findByNameNameContains(@Param(value = "name")String name);
     Optional<Professor> findByKeycloakId(@Param(value = "keycloakId") UUID keycloakId);
 
 }
--- src/main/java/io/archilab/prox/userprofileservice/student/StudentRepository.java
+++ src/main/java/io/archilab/prox/userprofileservice/student/StudentRepository.java
@@ -1,13 +1,14 @@
 package io.archilab.prox.userprofileservice.student;
 
 import io.archilab.prox.userprofileservice.professor.Professor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface StudentRepository extends JpaRepository<Student, Long>
+public interface StudentRepository extends JpaRepository<Student, UUID>
 {
     Optional<Student> findById(@Param(value = "id")UUID id);
     Optional<Student> findByKeycloakId(@Param(value = "keycloakId") UUID keycloakId);
