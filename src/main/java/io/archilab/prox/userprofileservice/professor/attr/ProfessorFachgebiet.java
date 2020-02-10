be5cf0266579df39738994a2c9c3d5121296113c
mmarienh <Marco.Marienhagen@googlemail.com>
2020-02-10 21:03:56 +0100

    V0_init und application.yml aktualisiert, ProfessorFachgebiet.java entfernt, InitRunner aktualisiert

--- src/main/java/io/archilab/prox/userprofileservice/professor/attr/ProfessorFachgebiet.java
+++
@@ -1,6 +0,0 @@
-package io.archilab.prox.userprofileservice.professor.attr;
-
-public enum ProfessorFachgebiet {
-
-    INFORMATIK, ELEKTROTECHNIK, MASCHINENBAU
-}
--- src/main/resources/db/migration/V0__init.sql
+++ src/main/resources/db/migration/V0__init.sql
@@ -1,17 +1,37 @@
 create table professor
 (
     id              uuid not null,
-    firstName       varchar(127),
-    lastName        varchar(127),
-    title           int4,
-    description     varchar(3000),
-    hasDescription  BOOLEAN,
+    keycloakId      uuid not null,
+    name           varchar(127),
+    title           varchar(50),
+    adresse         varchar(500),
+    strasse         varchar(500),
+    plz             varchar(500),
     raum            varchar(16),
-    telefonummer    bigint,
-    email           varchar (127),
-    sprechzeiten    varchar(64),
+    phoneNumber     varchar(50),
+    mail            varchar (127),
+    tags            varchar(3000),
+    sprechzeiten    varchar (3000),
+    aboutMe         varchar(3000),
+    hasAboutMe      BOOLEAN,
     bildSrc         varchar (255),
-    hasBild         BOOLEAN,
-    fachgebiet      int4,
+    hasBild         BOOLEAN
+    primary key (id)
+);
+
+create table student
+(
+    id              uuid not null,
+    keycloakId      uuid not null,
+    phoneNumber     varchar(50),
+    mail            varchar(127),
+    tags            varchar(3000),
+    aboutMe         varchar(3000),
+    hasAboutMe      BOOLEAN,
+    studiengang     varchar(100),
+    schwerpunkt     varchar(100),
+    qualifikation   varchar(3000),
+    doneModules     varchar(1000),
+    doneJobs        varchar(1000)
     primary key (id)
 );
