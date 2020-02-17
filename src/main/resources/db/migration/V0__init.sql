c4d0bf50df5ab93c0209963fed488b2cee01cd08
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2020-02-17 13:47:27 +0100

    Add working SQL init script

--- src/main/resources/db/migration/V0__init.sql
+++ src/main/resources/db/migration/V0__init.sql
@@ -1,37 +1,60 @@
-create table professor
+create table module
 (
-    id              uuid not null,
-    keycloakId      uuid not null,
-    name           varchar(127) not null,
-    title           varchar(50),
-    adresse         varchar(500),
-    strasse         varchar(500),
-    plz             varchar(500),
-    raum            varchar(16),
-    phoneNumber     varchar(50),
-    mail            varchar (127),
-    tags            text[],
-    sprechzeiten    varchar (3000),
-    aboutMe         varchar(3000),
-    hasAboutMe      BOOLEAN,
-    bildSrc         varchar (255),
-    hasBild         BOOLEAN,
+    id          uuid not null,
+    description varchar(9000),
+    name        varchar(255),
+    module_id   uuid,
     primary key (id)
 );
 
-create table student
+create table professor
 (
-    id              uuid not null,
-    keycloakId      uuid not null,
-    phoneNumber     varchar(50),
-    mail            varchar(127),
-    tags            varchar(3000),
-    aboutMe         varchar(3000),
-    hasAboutMe      BOOLEAN,
-    studiengang     varchar(100),
-    schwerpunkt     varchar(100),
-    qualifikation   varchar(3000),
-    doneModules     varchar(1000),
-    doneJobs        varchar(1000),
+    id           uuid not null,
+    about_me     varchar(255),
+    has_about_me boolean not null,
+    adresse      varchar(255),
+    bild_src     varchar(255),
+    has_bild     boolean not null,
+    keycloak_id  uuid,
+    mail         varchar(255),
+    name         varchar(255),
+    phonenumber  varchar(255),
+    plz          varchar(255),
+    raum         varchar(255),
+    sprechzeiten varchar(255),
+    strasse      varchar(255),
+    tags         bytea,
+    title        varchar(255),
     primary key (id)
 );
+
+create table student_table
+(
+    id            uuid not null,
+    about_me      varchar(255),
+    has_about_me  boolean not null,
+    done_jobs     varchar(255),
+    done_modules  bytea,
+    keycloak_id   uuid,
+    mail          varchar(255),
+    name          varchar(255),
+    phonenumber   varchar(255),
+    qualifikation varchar(255),
+    schwerpunkt   varchar(255),
+    status        varchar(255),
+    studiengang   varchar(255),
+    tags          bytea,
+    primary key (id)
+);
+
+create table study_course
+(
+    id                     uuid not null,
+    academic_degree        int4,
+    name                   varchar(255),
+    primary key (id)
+);
+
+alter table module
+  add constraint FK_module_study_course
+    foreign key (module_id) references study_course;
